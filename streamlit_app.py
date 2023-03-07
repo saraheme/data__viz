0c2ba798581b10bc1e4a4469be8334cdfc7fbfdf
saraheme <127039455+saraheme@users.noreply.github.com>
2023-03-07 01:08:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 import seaborn as sns
+import altair as alt 
 
 def df_aff(df):
     st.header('Affichage de la base de données')
@@ -17,15 +18,36 @@ def df_aff(df):
 
 def df_desc(df):
     st.header('Description du jeu de données')
-    st.write(df.describe())
+    st.write(df.describe()) #Description
+    #Sélection d'une variable pour afficher son histogramme et boxplot
+    st.write("Affichage histogramme et boxplot pour une variable donnée")
+    var_select = st.selectbox('Sélectionner une variable :', [col for col in df.columns if df[col].dtype != 'object'])
+
+    hist = alt.Chart(df).mark_bar().encode(
+        alt.X(var_select, bin=True),
+        y='count()',
+    ).properties(
+        width=600,
+        height=400,
+        title='Histogramme de {}'.format(var_select)
+    )
+    boxplot = alt.Chart(df).mark_boxplot().encode(
+        y=alt.Y(var_select),
+    ).properties(
+        width=600,
+        height=400,
+        title='Boxplot de {}'.format(var_select)
+    )
+    st.altair_chart(hist)
+    st.altair_chart(boxplot)
+
 
 def df_repartition(df):
-    # Titre de l'application
     st.header('Camembert des occurences dans une colonne')
-    # Sélection de la colonne
+    #Sélection de la colonne
     selected_column = st.selectbox('Sélectionnez une colonne pour afficher les occurences (on affiche le camembert s il y a moins de 20 occurences différentes)', list(df.columns))
 
-    # Affichage des occurences en camembert
+    #Affichage des occurences en camembert
     if selected_column is not None:
         nombre_differents = df[selected_column].nunique()
         if nombre_differents < 20:
@@ -41,31 +63,30 @@ def df_repartition(df):
 
 def interactive_plot(df):
     col1, col2 = st.columns(2)
-
-    x_axis_val = col1.selectbox("Sélectionnez l'axe X", options=df.columns)
-    y_axis_val = col2.selectbox("Sélectionnez l'axe Y", options=df.columns)
+    x_axis_val = col1.selectbox("Sélectionnez l'axe X", options=df.columns,key="plot_var1")
+    y_axis_val = col2.selectbox("Sélectionnez l'axe Y", options=df.columns,key="plot_var2")
     plot = px.scatter(df, x=x_axis_val, y=y_axis_val)
     st.plotly_chart(plot, use_container_width=True)
 
 
 
 def reg_lin_multiple(df):
-    df_quantitatif = df.select_dtypes(exclude=['object'])
+    df_quantitatif = df.select_dtypes(exclude=['object']) #On garde que les variables quantitatives
     selected_columns = st.multiselect('Sélectionnez les colonnes à inclure dans la régression (variables quantitatives)', list(df_quantitatif.columns))
     if len(selected_columns) >= 2:
-        # Sélection de la variable dépendante
-        target_column = st.selectbox('Sélectionnez la variable dépendante (Y)', selected_columns)
+        #Sélection de la variable à expliquer
+        variable_expliquee = st.selectbox('Sélectionnez la variable à expliquer (Y)', selected_columns)
 
-        # Sélection des variables indépendantes
-        feature_columns = [col for col in selected_columns if col != target_column]
+        #On stocke les variables explicatives
+        variables_explicatives = [col for col in selected_columns if col != variable_expliquee]
 
-        # Construction du modèle
-        X = df[feature_columns]
-        y = df[target_column]
+        #Modèle
+        X = df[variables_explicatives]
+        y = df[variable_expliquee]
 
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-        # Création d'un modèle de régression linéaire
+        # execution de la Régression linéaire
         reg = LinearRegression().fit(X_train, y_train)
 
         # Prédiction des valeurs de y pour les données de test
@@ -73,8 +94,8 @@ def reg_lin_multiple(df):
 
         # Affichage des résultats
         st.write("Coefficients : ")
-        for i in range(len(feature_columns)):
-            st.info(str(feature_columns[i]) + " : " +str(round(reg.coef_[i],3)))
+        for i in range(len(variables_explicatives)):
+            st.info(str(variables_explicatives[i]) + " : " +str(round(reg.coef_[i],3)))
         st.write("Moyenne quadratique des résidus (MQ) : %.2f" % np.mean((reg.predict(X_train) - y_train) ** 2))
         st.write("Score de prédiction (R²) : %.2f" % reg.score(X_train, y_train))
         st.write("Prédictions pour les données de test : ", y_pred)
@@ -84,14 +105,14 @@ def reg_lin_multiple(df):
 
         fig_test, (ax1_test, ax2_test) = plt.subplots(ncols=2, figsize=(10, 4))
         sns.regplot(x=y_test, y=reg.predict(X_test), ax=ax1_test)
-        ax1_test.set_xlabel(target_column)
+        ax1_test.set_xlabel(variable_expliquee)
         ax1_test.set_ylabel('Prédiction')
         ax1_test.set_title('Régression linéaire multiple')
 
         # Tracé des résidus
         residuals_test = y_test - reg.predict(X_test)
         sns.residplot(x=y_test, y=residuals_test, ax=ax2_test)
-        ax2_test.set_xlabel(target_column)
+        ax2_test.set_xlabel(variable_expliquee)
         ax2_test.set_ylabel('Résidus')
         ax2_test.set_title('Résidus de la régression linéaire multiple')
         st.pyplot(fig_test)
@@ -101,14 +122,14 @@ def reg_lin_multiple(df):
 
         fig_train, (ax1_train, ax2_train) = plt.subplots(ncols=2, figsize=(10, 4))
         sns.regplot(x=y_train, y=reg.predict(X_train), ax=ax1_train)
-        ax1_train.set_xlabel(target_column)
+        ax1_train.set_xlabel(variable_expliquee)
         ax1_train.set_ylabel('Prédiction')
         ax1_train.set_title('Régression linéaire multiple')
 
         # Tracé des résidus
         residuals_train = y_train - reg.predict(X_train)
         sns.residplot(x=y_train, y=residuals_train, ax=ax2_train)
-        ax2_train.set_xlabel(target_column)
+        ax2_train.set_xlabel(variable_expliquee)
         ax2_train.set_ylabel('Résidus')
         ax2_train.set_title('Résidus de la régression linéaire multiple')
         st.pyplot(fig_train)
@@ -116,64 +137,65 @@ def reg_lin_multiple(df):
 
 def correlation(df):
     st.title('Corrélation entre deux variables quantitatives du jeu de données')
-    # Sélection des colonnes
-    df_quantitatif = df.select_dtypes(exclude=['object'])
-    selected_columns = st.multiselect('Sélectionnez deux colonnes pour calculer la corrélation', list(df_quantitatif.columns))
+    #Sélection des colonnes
+    df_quantitatif = df.select_dtypes(exclude=['object']) #On garde que les variables quantitatives
+    st.info("Sélectionnez deux colonnes pour calculer la corrélation")
+    col1, col2 = st.columns(2)
+    x_axis_val = col1.selectbox("Choix variable : ", options=df_quantitatif.columns, key="corr_var1")
+    y_axis_val = col2.selectbox("Choix variable : ", options=df_quantitatif.columns, key="corr_var2")
+    selected_columns = [x_axis_val,y_axis_val]
+    #Calcul de la corrélation
+    corr = df[selected_columns].corr()
+    #Tracé de la heatmap de corrélation
+    fig, ax = plt.subplots()
+    sns.heatmap(corr, annot=True, cmap='coolwarm', ax=ax)
+    ax.set_title('Heatmap de corrélation')
+    st.pyplot(fig)
+    #Tracé de la courbe de corrélation
+    fig, ax = plt.subplots()
+    ax.plot(df[selected_columns[0]], df[selected_columns[1]], 'o')
+    ax.set_xlabel(selected_columns[0])
+    ax.set_ylabel(selected_columns[1])
+    ax.set_title('Courbe de corrélation')
+    st.pyplot(fig)
 
-    # Vérification que deux colonnes ont été sélectionnées
-    if len(selected_columns) == 2:
-        # Calcul de la corrélation
-        corr = df[selected_columns].corr()
-        # Tracé de la heatmap de corrélation
-        fig, ax = plt.subplots()
-        sns.heatmap(corr, annot=True, cmap='coolwarm', ax=ax)
-        ax.set_title('Heatmap de corrélation')
-        st.pyplot(fig)
-        #Tracé de la courbe de corrélation
-        fig, ax = plt.subplots()
-        ax.plot(df[selected_columns[0]], df[selected_columns[1]], 'o')
-        ax.set_xlabel(selected_columns[0])
-        ax.set_ylabel(selected_columns[1])
-        ax.set_title('Courbe de corrélation')
-        st.pyplot(fig)
-    else:
-        st.write('Veuillez sélectionner exactement deux colonnes.')
+
 
 
 def reg_logistique(df):
     selected_columns = st.multiselect(
         'Sélectionnez les colonnes à inclure dans la régression (variables quantitatives)',list(df.columns))
-    if len(selected_columns) >= 2:
-        # Sélection de la variable dépendante
-        target_column = st.selectbox('Sélectionnez la variable dépendante (Y)', selected_columns)
+    if len(selected_columns) >= 2: #On a une variable expliquée et au moins une variable explicative
+        #Sélection de la variable expliquée
+        variable_expliquee = st.selectbox('Sélectionnez la variable dépendante (Y)', selected_columns)
 
-        # Sélection des variables indépendantes
-        feature_columns = [col for col in selected_columns if col != target_column]
+        #Sélection des variables explicatives
+        variables_explicatives = [col for col in selected_columns if col != variable_expliquee]
 
-        # Construction du modèle
-        X = df[feature_columns]
-        y = df[target_column]
+        #Construction du modèle
+        X = df[variables_explicatives]
+        y = df[variable_expliquee]
 
 
-        # Diviser les données en ensembles d'entraînement et de test
+        #Diviser les données en ensembles d'entraînement et de test
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
 
-        # Créer un objet de modèle de régression logistique
+        #Créer un objet de modèle de régression logistique
         model = LogisticRegression()
 
-        # Entraîner le modèle sur les données d'entraînement
+        #Entraîner le modèle sur les données d'entraînement
         model.fit(X_train, y_train)
 
-        # Prédire les valeurs de sortie pour les données de test
+        #Prédire les valeurs de sortie pour les données de test
         y_pred = model.predict(X_test)
 
-        # Évaluer les performances du modèle
+        #Évaluer les performances du modèle
         accuracy = metrics.accuracy_score(y_test, y_pred)
         st.write("Accuracy (qualité de la prédiction):", accuracy)
 
         st.write("Coefficients : ")
-        for i in range(len(feature_columns)):
-            st.info(str(feature_columns[i]) + " : " + str(model.coef_[0][i]))
+        for i in range(len(variables_explicatives)):
+            st.info(str(variables_explicatives[i]) + " : " + str(model.coef_[0][i]))
 
         # Linear
         y_test_logistic = model.predict_proba(X=X_test)[:, 1]
@@ -214,9 +236,8 @@ upload_file = st.sidebar.file_uploader('Importez votre base de données')
 st.sidebar.title('Actions')
 #Sélection
 options = st.sidebar.radio('Quelle analyse voulez vous faire ?',
-                           ['Affichage de la base de données', 'Description du jeu de données','Répartition des valeurs de chaque colonne','Scatter Plot','Régression linéaire','Correlation','Régression logistique'])
+                           ['Affichage de la base de données', 'Description du jeu de données','Répartition des valeurs de chaque colonne','Scatter Plot','Correlation','Régression linéaire','Régression logistique'])
 if upload_file is None:
-
     st.text("Pour commencer importez une base de données")
 else:
     df = pd.read_csv(upload_file,sep=choix_separateur, encoding='latin-1',decimal= choix_decimal)
